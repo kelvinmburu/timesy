bae620b50eaaf51e567f8f8d71d1019c02a1e21c
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-18 16:25:28 +0300

    create protesqsql database

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from . import db
-from flask_login import UserMixin
+from flask_login import UserMixin,current_user
 from . import login_manager
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -19,11 +20,113 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
-    
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    timesy = db.relationship('Timesy', backref='user', lazy='dynamic')
     reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+    
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
     
     def __repr__(self):
         return f'User {self.username}'
+
+class Timesy(db.Model):
+    __tablename__ = 'timesy'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='timesy',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='timesy',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='timesy',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True,nullable = False)
+
+    def __repr__(self):
+            return f'User {self.username}'    
     
     
-# Other classes here
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+        
+    def __repr__(self):
+        return f'Timesy {self.post}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    timesy_id = db.Column(db.Integer,db.ForeignKey('timesy.id'),nullable = False)
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,timesy_id):
+        comments = Comment.query.filter_by(_id=timesy_id).all()
+
+        return comments
+
+    
+    def __repr__(self):
+        return f'comment:{self.comment}'
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    timesy_id = db.Column(db.Integer,db.ForeignKey('timesy.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvote.query.filter_by(timesy_id=id).all()
+        return upvote
+
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.timesy_id}'
+class Downvote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    timesy_id = db.Column(db.Integer,db.ForeignKey('timesy.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(timesy_id=id).all()
+        return downvote    
--- manage.py
+++ manage.py
@@ -1,20 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin,current_user
 
 app = Flask(__name__)
 #database instance
 db = SQLAlchemy(app)
 
 #connects app file to our database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] 
 app.config['SECRET_KEY']='thisisasecretkey'
-
-#database for tables
-class user (db,model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20),nullable=False)
-    password = db.Column(db.String(80),nullable=False)
-
 
 @app.route("/")
 def hello_world():
