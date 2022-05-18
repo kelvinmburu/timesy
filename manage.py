13511846fbf9153c5022012d651a11d2e4d4497e
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-18 12:58:04 +0300

    create database file

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#database instance
+db = SQLAlchemy(app)
+#connects app file to our database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
 
 @app.route("/")
 def hello_world():
