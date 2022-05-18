292605f5ee59423930e888bd3d7aabced691267d
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-18 13:20:15 +0300

    add secret login key

--- manage.py
+++ manage.py
@@ -7,6 +7,14 @@ db = SQLAlchemy(app)
 
 #connects app file to our database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
+app.config['SECRET_KEY']='thisisasecretkey'
+
+#database for tables
+class user (db,model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),nullable=False)
+    password = db.Column(db.String(80),nullable=False)
+
 
 @app.route("/")
 def hello_world():
