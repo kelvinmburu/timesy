13fbb8be71bf0e56bd4a7f48c9bb98b465d9b641
joycodes <jnguttu@gmail.com>
2022-05-19 07:58:34 +0300

    add redirect for user to profile

--- app/main/views.py
+++ app/main/views.py
@@ -15,7 +15,29 @@ def user():
         abort(404)
     return render_template('profile/profile.html', user=user)
 
+@main.route('/user/<name>/update_profile', methods=['POST', 'GET'])
+@login_required
+def update_profile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username=name).first()
+    if user is None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.user', name=user.username))
+    return render_template('profile/update_profile.html', form=form)
 
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.user',name=name))
 
 @main.route('/tasks')
 def tasks():
