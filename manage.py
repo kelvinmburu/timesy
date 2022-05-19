1cc88f78fb814ac5500e259ebbfa33c5d8919220
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-19 10:06:46 +0300

    build: installed all depedencies

--- manage.py
+++ manage.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User, Task, Reminder
+from  flask_migrate import Migrate, MigrateCommand
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+# Creating appinstance
+app = create_app('development')
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Task=Task, Reminder=Reminder )
+
+if __name__ == '__main__':
+    manager.run()
