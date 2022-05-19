6e0c92cedcc680f34c65cb3bd213a839bbda062c
joycodes <jnguttu@gmail.com>
2022-05-19 07:48:05 +0300

    add routing for user

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,18 @@ from flask_login import current_user
 from ..models import User
 
 #views
+
+@main.route('/user')
+@login_required
+def user():
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('profile/profile.html', user=user)
+
+
+
 @main.route('/tasks')
 def tasks():
   '''
